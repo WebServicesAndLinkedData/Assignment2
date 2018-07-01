d543f6bdbed7dad1025521e738dded4bb0a25556
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-07-01 23:00:56 +0200

    [ci skip]

--- build/comment.sh
+++ build/comment.sh
@@ -1,9 +1,9 @@
 if [ -s "err" ]
 then
   msg=$(sed ':a;N;$!ba;s/\n/<br\/>/g' err)
-  cat $msg
+  echo $msg
 else
   msg="Build Succesful"
 fi
 sleep 1
-curl -v -H "Authorization: token $TOKEN" -X POST -d "{\"body\": \" $msg \"}" "https://api.github.com/repos/${SEMAPHORE_REPO_SLUG}/issues/${PULL_REQUEST_NUMBER}/comments" > /dev/null
+curl -H "Authorization: token $TOKEN" -X POST -d "{\"body\": \" $msg \"}" "https://api.github.com/repos/${SEMAPHORE_REPO_SLUG}/issues/${PULL_REQUEST_NUMBER}/comments" > /dev/null
